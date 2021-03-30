32df7bc3a2df35f00a4abbe0543783515a5d282a
Edson Jordan <edson_4555@hotmail.com>
2021-03-30 14:28:01 -0500

    Comenzando curso completo laravel V8

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+    return "Hola mundo Iniciando Laravel";
+});
+Route::get('cursos', function () {
+    return "Bienvenidos a la pagina cursos";
+});

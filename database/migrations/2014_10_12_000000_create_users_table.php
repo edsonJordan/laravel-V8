1ec27a61444799c7ccffd9ae1a2a883bb707a3d6
Edson Jordan <edson_4555@hotmail.com>
2021-03-30 17:56:38 -0500

    Introducción a las migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id();//asiga id integrer increment
+            $table->string('name');//varchar  string 255 maximo
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();//created_at update_at
         });
     }
 

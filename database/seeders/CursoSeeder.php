5fc7772fa6118d8a9917bc0687fc9718bab5e7de
Edson Jordan <edson_4555@hotmail.com>
2021-03-31 12:47:37 -0500

    10 - Seeders en Laravel ¿Qué son y para que sirven?

---
+++ database/seeders/CursoSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Curso;
+use Illuminate\Database\Seeder;
+
+class CursoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $curso = new Curso();
+        $curso->name = "Laravel";
+        $curso->description = "El mejor framework de PHP";
+        $curso->categoria = "Desarrollo web";
+        $curso->save();
+
+        $curso2= new Curso();
+        $curso2->name = "Vue";
+        $curso2->description = "El mejor framework de Javascript";
+        $curso2->categoria = "Desarrollo web";
+        $curso2->save();
+    }
+}
